f8ea80de90f72265506f21ea4bcb97856ad366ed
jsingh214 <jsingh214@msn.com>
2014-01-28 10:58:10 -0500

    Users are now validated before logging in.
    Want to double check this before with echoes with Jeremy at some point
    but they should be working fine.

--- validation.php
+++ validation.php
@@ -11,19 +11,21 @@ if (empty($_POST['password'])) {
 $username = mysqli_real_escape_string($connection, $_POST["username"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 
-$userdata = mysqli_query(
-    $connection,
+$userdata = mysqli_query($connection,
     "SELECT * FROM members WHERE username='$username' and password='$password'"
 ) or die (mysqli_error($connection));
-$login = mysqli_fetch_array($userdata, MYSQL_ASSOC);
-mysqli_num_rows($login);
-if(mysqli_result() == 1) {
+$num_row = mysqli_num_rows($userdata);
+
+
+if($num_row != 1) {
+    header('Location: index.php');
+    echo "<p>You username or password is incorrect!</p>";
+} else {
+    while($row=mysqli_fetch_array($userdata) ){
     session_start();
     $_SESSION['username'] = $username;
     header('Location: home.php');
-} else {
-  //  header('Location: index.php');
-    echo "<p>You username or password is incorrect!</p>";
+    }
 }
 /*Closes the connection here because connection only stays open until user is validated and in.*/
 mysqli_close($connection);

dc90342afacaa08789311df13a52f5f63fe7ca3e
jsingh214 <jsingh214@msn.com>
2014-03-06 17:40:13 -0500

    Added future functions and imbedded dynamics.
    Dynamic form is now in a table with a border. Started a conversion if
    else for the binary off on for status.

--- home.php
+++ home.php
@@ -1,4 +1,19 @@
-<?php
+
+
+<!DOCTYPE html>
+<!--The home page contains a table where users can pick devices to configure.-->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="main/style.css">
+        <title>Home</title>
+    </head>
+    <a href="logout.php"><button>Logout</button> </a>
+    <body>
+    <header>
+	</header>
+        <form name='pods' action='pod_select.php'>
+        <?php
 require('connect_equipment.php');
 
 $check = true;
@@ -13,37 +28,34 @@ if($check==true) {
   $result_db = $connection->query($sql);
 
   if (!$result_db) {
-		echo $connection->error . ' Error perform query!';
+		echo $connection->error . ' Error performing query!';
   }
 
 
   else {
+  echo "<table border='1'>";
                  while ($row = $result_db->fetch_object()) {
-			echo "<input type=\"radio\" name=\"pod\" 
+                 /*$row->status = $status;
+                 if($status = 0)
+                 { $status = "Off"}
+                 else{$status = "On"}*/
+                 
+			echo "<tr><td><input type=\"radio\" name=\"pod\" 
                              value=\"{$row->pod_id}\" 
-                             id=\"id{$row->pod_id}\"/>";
-            echo "<label for=\"id{$row->pod_id}\">
-                                $row->pod_id</label>";
-			echo "<label for=\"id{$row->pod_desc}\">
-                                $row->pod_desc</label>";
-            echo "<label for=\"id{$row->status}\">
-                                $row->status</label><br/>";
+                             id=\"id{$row->pod_id}\"/></td>";
+            echo "<td><label for=\"id{$row->pod_id}\">
+                                $row->pod_id</label></td>";
+			echo "<td><label for=\"id{$row->pod_desc}\">
+                                $row->pod_desc</label></td>";
+            echo "<td><label for=\"id{$row->status}\">
+                                $row->status</label><br/></td></tr>";
 		}
+        echo "</table>";
 	}
 }
-
-$connection->close();
 ?>
 
-<!DOCTYPE html>
-<!--The home page contains a table where users can pick devices to configure.-->
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="main/style.css">
-        <title>Home</title>
-    </head>
-    <a href="logout.php"><button>Logout</button> </a>
-    <body>
+    <p><button>Select Pod</button></p>
+    </form>
     </body>
 </html>

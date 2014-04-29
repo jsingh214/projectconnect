611cb528f938f210f912202b680f7c52880ea533
jsingh214 <jsingh214@msn.com>
2014-04-28 21:59:17 -0400

    Fixed issue
    Missed updating validation page on server. It is updated and code
    currently works in connecting to anyterm.

--- pod_select.php
+++ pod_select.php
@@ -1,9 +1,12 @@
 <?php
+
 function checkoutPod($pod_id){
     require('connect_equipment.php');
     session_start();
     $mem_id = $_SESSION['mem_id'];   
-    if (!mysqli_query($connection, "UPDATE pods SET mem_id = ".$mem_id." WHERE pod_id = ".$pod_id))
+    $qry = "UPDATE pods SET mem_id = ".$mem_id." WHERE pod_id = ".$pod_id;
+
+    if (!mysqli_query($connection, $qry))
      {
      die('Error: ' . mysqli_error($connection));
      }
@@ -15,11 +18,11 @@ function checkoutPod($pod_id){
     if (isset($_POST["pod"])
     && $_POST["pod"] == "1") {
         checkoutPod($_POST["pod"]);
-        header('Location: anyterm-1.1.29/browser/anyterm.html');
+        header('Location: http://155.42.107.197:7778/anyterm.html');
     } else if (isset($_POST["pod"])
     && $_POST["pod"] == "2") {
         checkoutPod($_POST["pod"]);
-        header('Location: anyterm-1.1.29/browser/anyterm.html');
+        header('Location: http://155.42.107.197:7778/anyterm.html');
     } else {
         echo "Error!";
     }
